b4904664b7efd46504b6d7e468c8ca14497c857c
Antonio Marsari <antonio.marsari@gmail.com>
2026-09-07 08:39:42 -0300

    feat: place test specs into standard cypress/e2e folder

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+        baseURL: "https://www.saucedemo.com",
+        viewportWidth: 1280,
+        viewportHeight: 720,
+        video: false,
+        setupNodeEvents(on, config) {
+            //Implement node event listeners here
+        }
+    }
+})
